ab4135d07a294d86ab1519d7326e49a60ef0a0fe
Myrkri <pertsevoi@yahoo.com>
2025-02-05 23:31:37 +0200

    added Kafka and extracted db connection from k8s

---
+++ src/main/java/com/example/csv/service/impl/CsvRecordConsumer.java
@@ -0,0 +1,16 @@
+package com.example.csv.service.impl;
+
+import com.example.csv.model.CsvRecord;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class CsvRecordConsumer {
+
+    @KafkaListener(topics = "${kafka.topic.name}")
+    public void consumeRecord(CsvRecord csvRecord) {
+        log.info("Received CSV record from Kafka: {}", csvRecord.toString());
+    }
+}
--- src/main/java/com/example/csv/service/impl/FileUploadImpl.java
+++ src/main/java/com/example/csv/service/impl/FileUploadImpl.java
@@ -3,19 +3,30 @@ package com.example.csv.service.impl;
 import com.example.csv.model.CsvRecord;
 import com.example.csv.service.FileUpload;
 import com.github.mygreen.supercsv.io.LazyCsvAnnotationBeanReader;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.supercsv.prefs.CsvPreference;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class FileUploadImpl implements FileUpload {
+
+    private final KafkaTemplate<String, CsvRecord> kafkaTemplate;
+
+    @Value("${kafka.topic.name}")
+    private String topic;
 
     @Override
     public List<CsvRecord> upload(final MultipartFile file) {
@@ -24,13 +35,16 @@ public class FileUploadImpl implements FileUpload {
         try (LazyCsvAnnotationBeanReader<CsvRecord> csvReader = new LazyCsvAnnotationBeanReader<>(
                 CsvRecord.class,
                 new InputStreamReader(file.getInputStream()), CsvPreference.STANDARD_PREFERENCE)) {
-
             result = csvReader.readAll();
+
+            if (!CollectionUtils.isEmpty(result)) {
+                for (CsvRecord csvRecord : result) {
+                    kafkaTemplate.send(topic, csvRecord);
+                }
+            }
         } catch (IOException e) {
             log.error("Failed to read file", e);
         }
         return result;
     }
-
-
 }
